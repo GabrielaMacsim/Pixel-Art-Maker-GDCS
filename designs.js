5e1807ca2c2815de6f7fdd48f3aeb8e21cdb4a20
Gabriela Macsim <31318570+GabrielaMacsim@users.noreply.github.com>
2018-01-31 22:11:44 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,16 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
+  const plansa = $("pixel_canvas");
+  const height = $("input_height").val();
+  const width = $("input_width").val();
+  const color = $("colorPicker").val();
 
-// Your code goes here!
+ for (c=0; c<height; c++) {
+  plansa.append("<tr></tr>");
+ }
+const rows = $("tr");
+ for (d=0; d<width; d++) {
+  rows.append("<td></td>");
+ }
 
-}
+
+})
