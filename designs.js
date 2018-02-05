7ef53bd67c0dcd82d3c788add4094e2e9ed54539
Gabriela Macsim <31318570+GabrielaMacsim@users.noreply.github.com>
2018-02-05 11:31:31 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
+function cleanGrid() {
+  $("#pixel_canvas").empty()
+}
 function makeGrid() {
+  const plansa = $("#pixel_canvas");
+  const height = parseInt($("#input_height").val());
+  const width = parseInt($("#input_width").val());
 
-// Your code goes here!
+  // clean the table before drawing
+  cleanGrid();
 
+  var isMouseDown = false;
+  // draw the table
+  for (c=0; c<height; c++) {
+    plansa.append("<tr></tr>");
+  }
+  const rows = $("tr");
+  for (d=0; d<width; d++) {
+    // tdElem = $("<td>").on("click", colorElem);
+    let tdElem = $("<td>")
+     tdElem.mousedown(function () { isMouseDown = true });
+     tdElem.mouseup(function() { isMouseDown = false });
+     tdElem.hover(function(event) {
+       if (isMouseDown) {
+        $(event.target).css("background-color", $("#colorPicker").val())
+     }
+    })
+
+    rows.append(tdElem);
+  }
+}
+
+function colorElem(event) {
+    let currentColor = $("#colorPicker").val();
+    $(event.target).css("background-color", currentColor)
 }
